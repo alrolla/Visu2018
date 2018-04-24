d266c48aa3134824711a03b102f4e103b6398058
Alfredo Rolla <alfredoluisrolla@gmail.com>
2018-04-24 07:58:43 -0300

    Update db.php

--- Visu2018/php/db.php
+++ Visu2018/php/db.php
@@ -1,9 +1,10 @@
 <?
+ // Conexion a la base de datos
   date_default_timezone_set('America/Argentina/Buenos_Aires');
   $username = "root";
   $password = "";
   $host = "localhost:3306";
-  $database="TesisEspecializacion";
+  $database="Visu2018";
 
   $connection = mysqli_connect($host, $username, $password,$database);
 ?>
